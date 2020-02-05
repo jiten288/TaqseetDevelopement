47d74de555917877a1d2302fa775723c294e4df9
Jiten <Jitugupta.09@gmail.com>
2020-02-05 16:42:23 +0530

    Added constant class and updated batch file.

--- TaqseetReconcilationBatch/TaqseetReconcilationBatch.bat
+++ TaqseetReconcilationBatch/TaqseetReconcilationBatch.bat
@@ -3,5 +3,7 @@ rem This batch is to reverse the cancel transactions
 
 set method=%1
 set filepath=%2
-
-java -jar  %CD%\TaqseetReconciliationBatch.jar "%method%" "%filepath%"
+SET "JAVA_HOME=C:\Program Files\Java\jdk1.7.0_80"
+echo JAVA HOME: %JAVA_HOME%
+"%JAVA_HOME%\bin\java" -jar  %CD%\TaqseetReconciliationBatch.jar "%method%" "%filepath%"
+pause
